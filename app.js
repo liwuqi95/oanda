40042cf08315d108e5d89410a5455078fc2ab8cc
liwuqi95 <wuqi.li@mail.utoronto.ca>
2020-09-10 21:50:26 +0800

    minor fix

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const agent = require('./api/agent');
+const instruments_list = ['WTICO_USD'];
 
 let accountID = null;
-
 
 const main = async () => {
   let response0 = await agent.Account.list();
   accountID = response0.accounts[0].id;
 
-
-  // agent.Instrument.list().then((response) => console.log(response));
   agent.Trade.list(accountID).then((response) => console.log(response));
-  agent.Order.list(accountID).then((response) => console.log(response));
-  agent.Pricing.pricing(accountID, 'WTI').then(response => console.log(response));
+  agent.Pricing.pricing(accountID, 'WTICO_USD').then(response => console.log(response));
 }
 
 main();
