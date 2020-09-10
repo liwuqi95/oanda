ce3ba6b3ef4ab4213906ee066c1f52a2f1602d50
liwuqi95 <wuqi.li@mail.utoronto.ca>
2020-09-10 22:05:48 +0800

    minor fix

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const agent = require('./api/agent');
-const instruments_list = ['WTICO_USD'];
+
+let db = {
+  'WTICO_USD': {
+    bid: null,
+    ask: null
+  }
+};
 
 let accountID = null;
 
-const main = async () => {
+const fetchAccount = async () => {
   let response0 = await agent.Account.list();
   accountID = response0.accounts[0].id;
+}
+
+
+const fetchPrice = async (instrument) => {
+  agent.Pricing.pricing(accountID, instrument).then(response => {
+    db[instrument].bid = response.prices[0].closeoutBid;
+    db[instrument].ask = response.prices[0].closeoutAsk;
+  });
+
+  console.log(db);
+}
+
+const main = async () => {
+
+  await fetchAccount();
 
   agent.Trade.list(accountID).then((response) => console.log(response));
-  agent.Pricing.pricing(accountID, 'WTICO_USD').then(response => console.log(response));
+
+  fetchPrice('WTICO_USD');
 }
 
 main();
